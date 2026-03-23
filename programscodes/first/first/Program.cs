08ce77090f41534e6a9bf56a98ded9adc9cc3468
Vilvapriya Manivel <vilvapriyam@infinite.com>
2026-03-23 21:56:47 +0530

    DAY1

--- programscodes/first/first/Program.cs
+++ programscodes/first/first/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,26 @@ namespace first
     {
         static void Main(string[] args)
         {
+            /*int n = 125;
+            Console.WriteLine(n);
+            float f = n;
+            Console.WriteLine(f);
+            n = (int)f;
+            Console.WriteLine(n);
+            n = Convert.ToInt32(f);
+            Console.WriteLine(n);*/
+            print();
+            Program program = new Program();
+            program.display();
+            Console.Read();
+        }
+        static void print()
+        {
+            Console.WriteLine(" HELLO VP");
+        }
+         public void display()
+        {
+            Console.WriteLine("Non static method in same class");
         }
     }
 }
